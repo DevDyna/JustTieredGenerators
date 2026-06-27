216b2dd645eea0f2a809135ffdba691f3ef163b5
DevDyna <andrearisorti2003@gmail.com>
2026-06-27 14:20:13 +0200

    init

---
+++ src/main/java/com/synergy/justtieredgens/Main.java
@@ -0,0 +1,19 @@
+package com.synergy.justtieredgens;
+
+import com.synergy.justtieredgens.init.Material;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+
+@Mod(Main.MODULE_ID)
+public class Main {
+
+    public static final String MODULE_ID = "justtieredgens";
+
+    public Main(IEventBus bus, ModContainer c) {
+        Material.register(bus);
+        GameEvents.build(bus, c);
+    }
+
+}
---
+++ src/main/java/com/synergy/justtieredgens/init/Material.java
@@ -0,0 +1,16 @@
+package com.synergy.justtieredgens.init;
+
+import com.synergy.justtieredgens.init.types.*;
+
+import net.neoforged.bus.api.IEventBus;
+
+public class Material {
+        public static void register(IEventBus bus) {
+                zItems.register(bus);
+                zBlocks.register(bus);
+                zBlockEntities.register(bus);
+                zRecipeTypes.register(bus);
+        }
+
+
+}
