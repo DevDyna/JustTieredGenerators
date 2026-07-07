4a8d562c66581dc2cbaefcd658c1fdf076e6cedf
DevDyna <andrearisorti2003@gmail.com>
2026-07-07 02:48:10 +0200

    added tooltip

--- src/main/java/com/synergy/justtieredgens/common/ItemToolTipped.java
+++ src/main/java/com/synergy/justtieredgens/common/ItemToolTipped.java
@@ -2,11 +2,16 @@ package com.synergy.justtieredgens.common;
 
 import static com.synergy.justtieredgens.Main.MODULE_ID;
 
+import com.direwolf20.justdirethings.common.blocks.GeneratorFluidT1;
 import com.direwolf20.justdirethings.common.blocks.GeneratorT1;
-import com.synergy.justtieredgens.api.factory.basegenerator.BaseCoalGenBlock;
-import com.synergy.justtieredgens.init.builders.generators.blazegold.BlazeGoldCoalGenBlock;
-import com.synergy.justtieredgens.init.builders.generators.celestigem.CelestigemCoalGenBlock;
-import com.synergy.justtieredgens.init.builders.generators.eclipse_alloy.EclipseAlloyCoalGenBlock;
+import com.synergy.justtieredgens.api.factory.coal.BaseCoalGenBlock;
+import com.synergy.justtieredgens.api.factory.fluid.BaseFluidGenBlock;
+import com.synergy.justtieredgens.init.builders.coal.blazegold.BlazeGoldCoalGenBlock;
+import com.synergy.justtieredgens.init.builders.coal.celestigem.CelestigemCoalGenBlock;
+import com.synergy.justtieredgens.init.builders.coal.eclipse_alloy.EclipseAlloyCoalGenBlock;
+import com.synergy.justtieredgens.init.builders.fluid.blazegold.BlazeGoldFluidGenBlock;
+import com.synergy.justtieredgens.init.builders.fluid.celestigem.CelestigemFluidGenBlock;
+import com.synergy.justtieredgens.init.builders.fluid.eclipse_alloy.EclipseAlloyFluidGenBlock;
 
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.BlockItem;
@@ -25,16 +30,17 @@ public class ItemToolTipped {
                 // var isBlock = stack instanceof BlockItem;
                 var block = ((stack instanceof BlockItem bi) ? bi.getBlock() : null);
 
-                if (block instanceof GeneratorT1 && !(block instanceof BaseCoalGenBlock))
+                if ((block instanceof GeneratorT1 && !(block instanceof BaseCoalGenBlock))
+                                || block instanceof GeneratorFluidT1 && !(block instanceof BaseFluidGenBlock))
                         tip.add(OVER_THE_REGISTRY_ID, Component.translatable(MODULE_ID + ".multiplier.ferricore"));
 
-                if (block instanceof BlazeGoldCoalGenBlock)
+                if (block instanceof BlazeGoldCoalGenBlock || block instanceof BlazeGoldFluidGenBlock)
                         tip.add(OVER_THE_REGISTRY_ID, Component.translatable(MODULE_ID + ".multiplier.blazegold"));
 
-                if (block instanceof CelestigemCoalGenBlock)
+                if (block instanceof CelestigemCoalGenBlock || block instanceof CelestigemFluidGenBlock)
                         tip.add(OVER_THE_REGISTRY_ID, Component.translatable(MODULE_ID + ".multiplier.celestigem"));
 
-                if (block instanceof EclipseAlloyCoalGenBlock)
+                if (block instanceof EclipseAlloyCoalGenBlock || block instanceof EclipseAlloyFluidGenBlock)
                         tip.add(OVER_THE_REGISTRY_ID, Component.translatable(MODULE_ID + ".multiplier.eclipsealloy"));
 
         }
